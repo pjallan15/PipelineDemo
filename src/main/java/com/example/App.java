ad359d80af9789c39e1758ddf623b50212f326a3
Peter Allan <pjallan15@github.com>
2025-05-01 13:41:14 +0100

    AM-100 testing the java test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,12 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, CI!");
+        System.out.println("Testing the CI!");
     }
 
     public static int add(int a, int b) {
+        int test = 8
         return a + b;
     }
+
 }
